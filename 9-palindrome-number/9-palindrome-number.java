1892686ed2ccd4ecf5ef391848cf3c4809ee4955
Caitlin DeShazo-Couchot <83627919+caitlin-dc@users.noreply.github.com>
2022-07-05 23:25:29 -0700

    Time: 84 ms (5.11%), Space: 51.6 MB (5.19%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,12 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        /*Negative and even numbers cannot be palindromes.
-        if (x < 0 || x / 2 == 0) {
-            return false;
-        }*/
         String temp = "";
         String num = Integer.toString(x);
-        //Traverses through number, adding last digit to temp.
         for (int i = 1; i <= num.length(); i++) {
             temp += num.charAt(num.length() - i);
         }
