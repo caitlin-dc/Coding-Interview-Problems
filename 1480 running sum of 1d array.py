fbf8e26666933992e33a7a9809c0164e7daa802d
Caitlin DeShazo-Couchot <83627919+caitlin-dc@users.noreply.github.com>
2023-02-21 23:28:20 -0800

    Create 1480 running sum of 1d array.py

---
+++ 1480 running sum of 1d array.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def runningSum(self, nums):
+        running_sum=[]
+        for i in range(len(nums)):
+            running_sum.append(sum(nums[:i+1]))
+        return running_sum 
