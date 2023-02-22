abb27943d7ed56c4dd3e92baefc159ad31cd6824
Caitlin DeShazo-Couchot <83627919+caitlin-dc@users.noreply.github.com>
2023-02-21 23:29:58 -0800

    Delete 1480 running sum of 1d array.py

--- 1480 running sum of 1d array.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def runningSum(self, nums):
-        running_sum=[]
-        for i in range(len(nums)):
-            running_sum.append(sum(nums[:i+1]))
-        return running_sum 
