9d188bc244a12d442232d8db9d70412d7d5293ea
Caitlin DeShazo-Couchot <83627919+caitlin-dc@users.noreply.github.com>
2023-11-02 12:32:29 -0700

    Time: 15 ms (50.15%), Space: 13.5 MB (8.28%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        i = 0
+        j = 0
+        word = ''
+        while (i < len(word1) and j < len(word2)):
+            word += word1[i]
+            word += word2[i]
+            i+=1
+            j+=1
+        
+        if (i < len(word1)):
+            word += word1[i: len(word1)]
+
+        elif (j < len(word2)):
+            word += word2[i: len(word2)]
+
+        return word
+
+        
