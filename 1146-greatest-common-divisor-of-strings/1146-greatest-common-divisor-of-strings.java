b16bd639caf261f213c9e76c65b145a839759ce5
Caitlin DeShazo-Couchot <83627919+caitlin-dc@users.noreply.github.com>
2023-11-05 10:36:24 -0800

    Time: 72 ms (5.02%), Space: 43.2 MB (13.88%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        // Find the GCD of both string lengths
+        int gcd = 0;
+        for (int i = str2.length(); i >= 0; i--) {
+            System.out.println(str1.length() % i);
+            System.out.println(str2.length() % i);
+            if ((str1.length() % i == 0) && (str2.length() % i == 0)) {
+                gcd = i;
+                break;
+            }
+        }
+        String x = "";
+        if (gcd >= 1) {
+            x = str2.substring(0,gcd);
+        }
+        return x;
+    }
+}
