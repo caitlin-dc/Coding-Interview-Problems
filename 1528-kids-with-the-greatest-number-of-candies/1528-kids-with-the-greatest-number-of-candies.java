4bf70f96d74509ad239dd3e5b7c769eb22956011
Caitlin DeShazo-Couchot <83627919+caitlin-dc@users.noreply.github.com>
2023-11-05 16:22:53 -0800

    Time: 2 ms (17.21%), Space: 40.7 mb (98.62%)

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -8,7 +8,7 @@ class Solution {
             if (candy + extraCandies >= max) {
                 mostCandies.add(true);
             }
-            else {
+            else { 
                 mostCandies.add(false);
             }
         }
